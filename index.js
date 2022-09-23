353198cd88d650aca3bc206399f5add74aec433c
Martin <souzam@sonic.net>
2022-09-23 14:54:05 -0400

    commit tree 3a63ba088774e87506f90d214dcf99cca9874cd4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+}
